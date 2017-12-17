1ab970eeb8e3a1132fbbaec69cf181dc2dcdff0a
davimeloni <dsmeloni@gmail.com>
2017-12-17 16:31:14 -0200

    adding postgreSQL

--- src/main/java/com/davimeloni/eventapp/DataConfiguration.java
+++ src/main/java/com/davimeloni/eventapp/DataConfiguration.java
@@ -1,7 +1,11 @@
 package com.davimeloni.eventapp;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -12,6 +16,23 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Configuration
 public class DataConfiguration {
 	
+	@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+	
+	/* MySQL
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -35,6 +56,7 @@ public class DataConfiguration {
 		
 		return adapter;
 	}
+	*/
 
 }
 
